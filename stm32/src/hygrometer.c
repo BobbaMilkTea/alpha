f4f17984006f6bd8161c942223e7b46fe64ea53c
BobbaMilkTea <btea7art@gmail.com>
2021-07-31 01:48:18 -0500

    no mg

--- stm32/src/hygrometer.c
+++ stm32/src/hygrometer.c
@@ -7,8 +7,7 @@
 
 unsigned char dataBuffer[5];
 int bufferPtr = 0;
-int respSignalFlag = 0;
-int dataTransmissionFlag = 0;
+int commPhase = 0; // 0: before resp from DHT11, 1: during resp from DHT11, 2: after resp from DHT11 (data transfer)
 
 static void _insert_buffer(int bit) {
 	if (bufferPtr >= 40)
@@ -30,10 +29,9 @@ static void _enable_EXTI() {
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 	// enable EXTI for PA0 in system config
 	SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI0;
-	// set EXTI to trigger on both rising and falling edge
-	EXTI->RTSR |= EXTI_RTSR_TR0;
+	// set EXTI to trigger on falling edge
 	EXTI->FTSR |= EXTI_FTSR_TR0;
-	// mask interrupt for the EXTI
+	// mask interrupt for EXTI
 	EXTI->IMR &= ~EXTI_IMR_MR0;
 	// enable interrupt in NVIC
 	NVIC->ISER[0] |= 1 << EXTI0_1_IRQn;
@@ -45,57 +43,42 @@ void hygrometer_init() {
 	// set data pin to output mode
 	GPIOA->MODER &= ~GPIO_MODER_MODER0_1;
 	GPIOA->MODER |= GPIO_MODER_MODER0_0;
-	// set data pin to pull up
-	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPDR0_1;
-	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR0_0;
 	// set data pin to output high
 	GPIOA->ODR |= GPIO_ODR_0;
 	_enable_EXTI();
 	tim6_init();
 }
 
-static void _falling_edge_handler() {
-	if(respSignalFlag && !dataTransmissionFlag) {
-		if(value_check(tim6_stop(), 80, 5))
-			dataTransmissionFlag = 1;
-	}
-	else if(respSignalFlag && dataTransmissionFlag) {
-		// determine bit logic level
-		int micro_elapsed = tim6_stop();
-		if(value_check(micro_elapsed, 27, 5))
+void EXTI0_1_IRQHandler() {
+	int micro_elapsed = tim6_stop();
+	tim6_start();
+	switch(commPhase) {
+	case 0: commPhase += 1;
+		break;
+	case 1: commPhase += value_check(micro_elapsed, 160, 10);
+		break;
+	case 2:
+		if(value_check(micro_elapsed, 77, 5))
 			_insert_buffer(0);
-		else if(value_check(micro_elapsed, 70, 5))
+		else if(value_check(micro_elapsed, 120, 5))
 			_insert_buffer(1);
 	}
-	tim6_start();
-}
-
-static void _rising_edge_handler() {
-	if(!respSignalFlag) {
-		if(value_check(tim6_stop(), 80, 5))
-			respSignalFlag = 1;
-	}
-	tim6_start();
-}
-
-void EXTI4_15_IRQnHandler() {
 	EXTI->PR |= EXTI_PR_PR0;
-	if(GPIOA->IDR & GPIO_IDR_8)
-		_rising_edge_handler();
-	else
-		_falling_edge_handler();
 }
 
 void hygrometer_read(short* temp, short* humidity) {
-	// enable interrupt for the EXTI
-	EXTI->IMR |= EXTI_IMR_MR0;
+	// send data request
 	GPIOA->ODR &= ~GPIO_ODR_0;
 	micro_wait(20000);
 	GPIOA->ODR |= GPIO_ODR_0;
 	// set data pin to input mode
 	GPIOA->MODER &= ~GPIO_MODER_MODER0;
+	// enable interrupt for the EXTI
+	EXTI->IMR |= EXTI_IMR_MR0;
 	// wait for data transmission to complete
 	while(bufferPtr < 40);
+	// mask interrupt when output received
+	EXTI->IMR &= ~EXTI_IMR_MR0;
 	// set data pin to output mode
 	GPIOA->MODER &= ~GPIO_MODER_MODER0_1;
 	GPIOA->MODER |= GPIO_MODER_MODER0_0;
@@ -107,8 +90,6 @@ void hygrometer_read(short* temp, short* humidity) {
 	*humidity = dataBuffer[0] << 8 | dataBuffer[1];
 	*temp = dataBuffer[2] << 8 | dataBuffer[3];
 	// reset status
-	respSignalFlag = 0;
-	dataTransmissionFlag = 0;
+	commPhase = 0;
 	_clear_buffer();
-	EXTI->IMR &= ~EXTI_IMR_MR0;
 }
